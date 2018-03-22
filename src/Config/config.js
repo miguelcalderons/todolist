967d519912ea9eb396ad819acb2cf94da4487e23
miguelcalderons <miguelcalderon10@gmail.com>
2018-03-22 09:20:38 -0400

    Update config js

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,10 +1,10 @@
 
   // Initialize Firebase
   export const DB_CONFIG = {
-    apiKey: "AIzaSyDvccpTMQpRIb2UPBb-Lz0qk1O7fsLoq7Q",
-    authDomain: "note-5c521.firebaseapp.com",
-    databaseURL: "https://note-5c521.firebaseio.com",
-    projectId: "note-5c521",
-    storageBucket: "note-5c521.appspot.com",
-    messagingSenderId: "651231896252"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   };
